bd6291292a6f1ab5ab6f4e5c790c32b9eda2960e
elemate <youto8023@outlook.com>
2016-12-04 19:27:55 +0800

    2016年12月4日 19:27:37 登录

---
+++ SSHShop/src/com/wq/action/IndexAction.java
@@ -0,0 +1,11 @@
+package com.wq.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class IndexAction extends ActionSupport {
+	
+	public String execute(){
+		return "index";
+	}
+}
